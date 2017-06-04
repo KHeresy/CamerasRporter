7b47b8e8ecbf85d13f7ac86c900d9e286df340af
Heresy <kheresy@gmail.com>
2017-06-04 19:26:09 +0800

    First edition that can play video

--- CamerasRporter/CamerasRporter.cpp
+++ CamerasRporter/CamerasRporter.cpp
@@ -1,7 +1,76 @@
 #include "CamerasRporter.h"
 
+#include <QDir>
+#include <QFileDialog>
+#include <QMessageBox>
+
+CCameraInfo::CCameraInfo()
+{
+	m_pPlayer = new QMediaPlayer();
+}
+
+bool CCameraInfo::setPath(const QString & sPath)
+{
+	m_sPath = sPath + m_sFolder;
+	QDir dirFolder = m_sPath;
+	if (dirFolder.exists())
+	{
+		m_vFileList = dirFolder.entryList({ "*.mp4" }, QDir::Files, QDir::Name);
+		m_pComboBox->clear();
+		m_pComboBox->addItems(m_vFileList);
+		return true;
+	}
+	return false;
+}
+
+void CCameraInfo::play(int iIdx)
+{
+	m_pPlayer->setMedia(QUrl::fromLocalFile(m_sPath + m_vFileList[iIdx]));
+	m_pPlayer->play();
+	auto s = m_pPlayer->errorString();
+}
+
 CamerasRporter::CamerasRporter(QWidget *parent)
 	: QMainWindow(parent)
 {
 	ui.setupUi(this);
+
+	aCameraInfo[0].m_sFolder = "/VIDA/";
+	aCameraInfo[0].m_pComboBox = ui.comboFileList1;
+	aCameraInfo[0].m_pPlayer->setVideoOutput(ui.videoCamera1);
+
+	aCameraInfo[1].m_sFolder = "/VIDB/";
+	aCameraInfo[1].m_pComboBox = ui.comboFileList2;
+	aCameraInfo[1].m_pPlayer->setVideoOutput(ui.videoCamera2);
+}
+
+void CamerasRporter::slotOpenFolder()
+{
+	QString sFolderName = QFileDialog::getExistingDirectory(this, tr("Select the video folder"), "");
+	if (sFolderName != "")
+	{
+		ui.labelInfo->setText(tr("Path: ") + sFolderName);
+
+		for (CCameraInfo& sCameraInfo : aCameraInfo)
+		{
+			if (sCameraInfo.setPath( sFolderName))
+			{
+
+			}
+			else
+			{
+
+			}
+		}
+	}
+}
+
+void CamerasRporter::slotCamera1Changed(int iIdx)
+{
+	aCameraInfo[0].play(iIdx);
+}
+
+void CamerasRporter::slotCamera2Changed(int iIdx)
+{
+	aCameraInfo[1].play(iIdx);
 }
--- CamerasRporter/CamerasRporter.h
+++ CamerasRporter/CamerasRporter.h
@@ -1,7 +1,28 @@
 #pragma once
 
+#include <array>
+
+#include <QComboBox>
+#include <QFileInfo>
+#include <QMediaPlayer>
 #include <QtWidgets/QMainWindow>
+
 #include "ui_CamerasRporter.h"
+
+class CCameraInfo
+{
+public:
+	QString			m_sFolder;
+	QString			m_sPath;
+	QStringList		m_vFileList;
+	QComboBox*		m_pComboBox;
+	QMediaPlayer*	m_pPlayer;
+
+public:
+	CCameraInfo();
+	bool setPath(const QString& sPath);
+	void play(int iIdx);
+};
 
 class CamerasRporter : public QMainWindow
 {
@@ -10,6 +31,12 @@ class CamerasRporter : public QMainWindow
 public:
 	CamerasRporter(QWidget *parent = Q_NULLPTR);
 
+public slots:
+	void slotOpenFolder();
+	void slotCamera1Changed(int);
+	void slotCamera2Changed(int);
+
 private:
-	Ui::CamerasRporterClass ui;
+	Ui::CamerasRporterClass		ui;
+	std::array<CCameraInfo, 2>	aCameraInfo;
 };
